b59e6bc9554d6371be60eae2263e0de49be0ddc3
Raj kuamr <rajkumar3205@gmail.com>
2022-02-16 22:44:01 +0530

    task models added

--- task/models.py
+++ task/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils.timezone import now
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Organization(models.Model):
+    name = models.CharField(max_length=50)
+
+class TodayYesterdayUpdate(models.Model):
+    user = models.ForeignKey(User,null=True, on_delete=models.CASCADE)
+    today_update = models.TextField()
+    yesterday_update = models.TextField()
+    blocker = models.TextField()
+    time = models.TimeField(default=now)
+    date = models.DateField(default=now)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
