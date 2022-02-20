a0b5c6ff80f2f2634944bd35ea841190bb0da38d
Raj kuamr <rajkumar3205@gmail.com>
2022-02-20 20:57:20 +0530

    -

--- task/urls.py
+++ task/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . views import task_view
+from . views import task_view, login_view, logout_view
 
 urlpatterns = [
-    path('', task_view, name="task")
+    path('', login_view, name="login"),
+    path('logout/', logout_view, name="logout"),
+    path('task/', task_view, name="task"),
 ]
--- task/views.py
+++ task/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . models import TodayYesterdayUpdate, Organization
 from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('/task/')
+    if request.method == "GET":
+        return render(request, 'login.html')
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        u = authenticate(request, username=username, password=password)
+        if u is not None:
+            login(request, u)
+            return redirect('/task/')
+    return render(request, 'login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'login.html')
+
+@login_required(login_url='/')
 def task_view(request):
     user = request.user
     orgs = Organization.objects.all()
@@ -12,4 +35,4 @@ def task_view(request):
             organization = org
             break
     tasks = TodayYesterdayUpdate.objects.filter(organization=organization)
-    return HttpResponse(tasks)
+    return render(request, 'task.html', {'tasks': tasks})
